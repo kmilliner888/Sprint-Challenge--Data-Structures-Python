24fdcb6c0cd86274206083eadb59cb68a21f6ce0
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-08-02 23:45:56 -0400

    created Binary Search Tree for names lists

--- names/names.py
+++ names/names.py
@@ -10,13 +10,38 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+class BST:
+    def __init__(self, name):
+        self.name = name
+        self.left = names_1
+        self.right = names_2
+
+    def contains(self, n):
+        if n:
+            return n
+        else:
+            return None
+
+    def finddups(self):
+        BSTList = []
+        for name_2 in names_2:
+            BSTList.append(name_2)
+
+        for name_1 in names_1:
+            if BSTList.contains(name_1):
+                duplicates.append(name_1)
+
+
+
+
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
